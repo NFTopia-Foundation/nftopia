35e254ed30fe9695f28c0d00cbd29c0c56eff65d
feyishola <feyisholaologunebi@gmail.com>
2025-04-27 06:52:19 +0100

    updated jwt strategy to handle error

--- apps/backend/src/auth/strategies/jwt.strategy.ts
+++ apps/backend/src/auth/strategies/jwt.strategy.ts
@@ -8,17 +8,25 @@ import { UsersService } from '../../users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+
   constructor(
     private configService: ConfigService,
     private usersService: UsersService,
   ) {
+    const jwtSecret = configService.get<string>('JWT_SECRET');
+    console.log('JWT_SECRET:', jwtSecret); // Debug log
+    
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET is not defined in environment variables');
+    }
+    
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
           return request?.cookies?.accessToken;
         },
       ]),
-      secretOrKey: configService.get<string>('JWT_SECRET'),
+      secretOrKey: jwtSecret,
       ignoreExpiration: false,
     });
   }
