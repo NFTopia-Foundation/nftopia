edf0a2eef665b718316703b313a34f5a4fd18f9b
Isenewo Oluwaseyi Ephraim <isenewoephr2012@gmail.com>
2025-04-29 16:11:13 +0100

    feat: Implemented the NftModule to enhance the minting of NFTs into NFT.Storage close #16

--- apps/backend/src/nfts/nfts.module.ts
+++ apps/backend/src/nfts/nfts.module.ts
@@ -4,11 +4,13 @@ import { NFT } from './entities/nft.entity';
 import { Collection } from '../collections/entities/collection.entity';
 import { NftsService } from './nfts.service';
 import { NftsController } from './nfts.controller';
+import { FirebaseModule } from '../firebase/firebase.module';
+import { NftStorageService } from '../nftstorage/nftstorage.service';
 import { UsersModule } from '../users/users.module';
 import { CollectionsModule } from '../collections/collections.module';
+import { NftStorageModule } from '../nftstorage/nftstorage.module';
 import { Category } from 'src/categories/entities/category.entity';
-import { CategoriesModule } from '../categories/categories.module';
-import { FirebaseModule } from '../firebase/firebase.module';
+import { CategoriesModule } from 'src/categories/categories.module';
 
 @Module({
   imports: [
@@ -16,9 +18,10 @@ import { FirebaseModule } from '../firebase/firebase.module';
     UsersModule, 
     FirebaseModule,
     CollectionsModule,
+    NftStorageModule,
     CategoriesModule
   ],
   controllers: [NftsController],
-  providers: [NftsService],
+  providers: [NftsService, NftStorageService],
 })
 export class NftsModule {}
