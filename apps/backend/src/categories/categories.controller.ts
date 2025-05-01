0c3229c198cb7c52e2e6b67c2b7f5a6817855565
DIVINE IFEDIORAH <ifediorahchinonso5@gmail.com>
2025-05-01 12:04:56 +0100

    nft category stats implementation

--- apps/backend/src/categories/categories.controller.ts
+++ apps/backend/src/categories/categories.controller.ts
@@ -1,9 +1,31 @@
-// src/categories/category.controller.ts
-import { Controller } from '@nestjs/common';
-
-
-
-@Controller('categories')
-export class CategoriesController {
- 
-}
+import {
+     Controller, Post, Body, Get, Param, Put
+   } from '@nestjs/common';
+import { CategoryService } from './categories.service';
+import {  UpdateCategoryDto } from './dto/update-category.dto';
+import { CreateCategoryDto } from './dto/create-category.dto';
+   @Controller('categories')
+   export class CategoryController {
+     constructor(private readonly service: CategoryService) {}
+   
+     @Post()
+     create(@Body() dto: CreateCategoryDto) {
+       return this.service.create(dto);
+     }
+   
+     @Put(':id')
+     update(@Param('id') id: string, @Body() dto: UpdateCategoryDto) {
+       return this.service.update(id, dto);
+     }
+   
+     @Get()
+     findAll() {
+       return this.service.findAll();
+     }
+   
+     @Get(':id')
+     findOne(@Param('id') id: string) {
+       return this.service.findOne(id);
+     }
+   }
+   
--- apps/backend/src/categories/categories.service.ts
+++ apps/backend/src/categories/categories.service.ts
@@ -1,7 +1,37 @@
-// src/categories/category.service.ts
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
+import { CreateCategoryDto,} from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 @Injectable()
-export class CategoriesService {
-  
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepo: Repository<Category>,
+  ) {}
+
+  async create(dto: CreateCategoryDto) {
+    const category = this.categoryRepo.create(dto);
+    return await this.categoryRepo.save(category);
+  }
+
+  async update(id: string, dto: UpdateCategoryDto) {
+    const category = await this.categoryRepo.preload({ id: Number(id), ...dto });
+    if (!category) throw new NotFoundException('Category not found');
+    return this.categoryRepo.save(category);
+  }
+
+  async findAll() {
+    return this.categoryRepo.find({ relations: ['nfts'] });
+  }
+
+  async findOne(id: string) {
+    const category = await this.categoryRepo.findOne({
+      where: { id: Number(id) },
+      relations: ['nfts'],
+    });
+    if (!category) throw new NotFoundException('Category not found');
+    return category;
+  }
 }
