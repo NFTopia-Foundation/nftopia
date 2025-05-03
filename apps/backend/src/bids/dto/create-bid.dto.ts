90bca5834f2da82c4c86ad3297ba5838cbcc81a0
njokojunior <njokojunior1@gmail.com>
2025-05-03 06:06:25 +0200

    Created bid module

--- apps/backend/src/bids/dto/create-bid.dto.ts
+++ apps/backend/src/bids/dto/create-bid.dto.ts
@@ -1,21 +1,10 @@
-// bid/dto/create-bid.dto.ts
-import { IsUUID, IsNumber } from 'class-validator';
+import { IsUUID, IsNumber, Min } from 'class-validator';
 
 export class CreateBidDto {
   @IsUUID()
   auctionId: string;
 
   @IsNumber()
+  @Min(0)
   amount: number;
 }
-
-
-// bid/dto/bid-response.dto.ts
-export class BidResponseDto {
-    id: string;
-    auctionId: string;
-    bidderId: string;
-    amount: number;
-    createdAt: string;
-  }
-  
