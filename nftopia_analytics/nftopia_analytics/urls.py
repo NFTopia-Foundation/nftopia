5b53841becc11b4cd1d6a5fa7b5ba69fca89b60f
Dagdon <dagdonjephthah@gmail.com>
2025-06-30 13:38:46 +0100

    updates

--- nftopia_analytics/nftopia_analytics/urls.py
+++ nftopia_analytics/nftopia_analytics/urls.py
@@ -14,15 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from marketplace.views import GasMintingView, GasSalesView, GasPredictionsView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('analytics/', include('analytics.urls')),
-    path('api/gas/minting/', GasMintingView.as_view(), name='gas-minting'),
-    path('api/gas/sales/', GasSalesView.as_view(), name='gas-sales'),
-    path('api/gas/predictions/', GasPredictionsView.as_view(), name='gas-predictions'),
-    path('api/auth/', include('authentication.urls')),
+    path("admin/", admin.site.urls),
+    path("analytics/", include("analytics.urls")),
+    path("api/gas/minting/", GasMintingView.as_view(), name="gas-minting"),
+    path("api/gas/sales/", GasSalesView.as_view(), name="gas-sales"),
+    path("api/gas/predictions/", GasPredictionsView.as_view(), name="gas-predictions"),
+    path("api/auth/", include("authentication.urls")),
 ]
