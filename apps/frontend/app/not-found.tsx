a7b185c0bbc4c00f246da57548eff468f6cba25d
martinvibes <martinmachiebe21@gmail>
2025-07-24 14:28:22 +0100

    feat: enhance NotFound page responsiveness and update button component

--- apps/frontend/app/not-found.tsx
+++ apps/frontend/app/not-found.tsx
@@ -3,24 +3,46 @@
 import LottiePlayer from "@/components/animations/LottiePlayer";
 import Link from "next/link";
 import React from "react";
+import { useMobile } from "@/hooks";
+import { Button } from "@/components/ui/button";
 
 const NotFound = () => {
+  const isMobile = useMobile();
+
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex flex-col space-y-3 ml-48">
-        <h1 className="text-6xl font-bold">Oops!</h1>
-        <p className="text-4xl">
+    <div
+      className={`flex items-center justify-center min-h-[60vh] w-full px-4 py-12 ${
+        isMobile ? "flex-col space-y-8" : "flex-row space-x-8"
+      }`}
+    >
+      <div
+        className={`flex flex-col ${
+          isMobile
+            ? "items-center text-center space-y-4"
+            : "space-y-3 max-w-md items-start text-left"
+        }`}
+      >
+        <h1
+          className={
+            isMobile ? "text-4xl font-bold mt-5" : "text-6xl mt-5 font-bold"
+          }
+        >
+          Oops!
+        </h1>
+        <p className={isMobile ? "text-lg" : "text-2xl"}>
           We can’t seem to find the page you’re looking for.
         </p>
-        <p className="text-4xl">Error code: 404</p>
+        <p className={isMobile ? "text-lg" : "text-2xl"}>Error code: 404</p>
         <Link href="/">
-          <button className="rounded-xl px-6 py-4 bg-gradient-to-r from-[#4e3bff] to-[#9747ff] text-white hover:opacity-90">
+          <Button
+            size={isMobile ? "sm" : "lg"}
+            className="rounded-xl bg-gradient-to-r from-[#4e3bff] to-[#9747ff] text-white mt-2"
+          >
             Back to Homepage
-          </button>
+          </Button>
         </Link>
       </div>
-
-      <div className=" -ml-48 ">
+      <div className={isMobile ? "w-full max-w-2xl mt-8" : "w-[740px] ml-8"}>
         <LottiePlayer />
       </div>
     </div>
